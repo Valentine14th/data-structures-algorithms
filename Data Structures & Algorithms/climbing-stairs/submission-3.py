a9fceec5f832c6e38cb360726756a14bbf0abedd
Valentine Tscharner <valentine.tscharner@hotmail.com>
2026-04-04 18:16:31 +0200

    Add: climbing-stairs - submission-3

---
+++ Data Structures & Algorithms/climbing-stairs/submission-3.py
@@ -0,0 +1,14 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        steps = [-1]*(n+1)
+        def rec(curr):
+            if curr > n:
+                return 0
+            if curr == n:
+                return 1
+            if steps[curr] != -1:
+                return steps[curr]
+            steps[curr] = rec(curr+2) + rec(curr+1)
+            return steps[curr]
+        return rec(0)
+        
